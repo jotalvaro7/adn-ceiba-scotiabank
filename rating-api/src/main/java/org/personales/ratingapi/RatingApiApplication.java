80bf2709666deb6617633b4d3fb5114578e1833c
jotalvaro7 <jotalvaro.cc@gmail.com>
2022-11-09 09:24:30 -0500

    iniciando proyecto

---
+++ rating-api/src/main/java/org/personales/ratingapi/RatingApiApplication.java
@@ -0,0 +1,13 @@
+package org.personales.ratingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingApiApplication.class, args);
+	}
+
+}
---
+++ rating-api/src/test/java/org/personales/ratingapi/RatingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personales.ratingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
