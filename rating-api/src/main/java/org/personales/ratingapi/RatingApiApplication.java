46f6448c864153209884336fe91708774b0f1caf
jotalvaro7 <jotalvaro.cc@gmail.com>
2022-11-14 13:44:38 -0500

    Implementando dependecias a los msvcs para ser cliente de eureka, inyectando decorador en el main y probando

--- rating-api/src/main/java/org/personales/ratingapi/RatingApiApplication.java
+++ rating-api/src/main/java/org/personales/ratingapi/RatingApiApplication.java
@@ -2,8 +2,10 @@ package org.personales.ratingapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingApiApplication {
 
 	public static void main(String[] args) {
