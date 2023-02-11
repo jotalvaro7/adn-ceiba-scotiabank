be13105d29f03a9f847d84ed2f6ecc73f5756547
jotalvaro7 <jotalvaro.cc@gmail.com>
2023-02-11 10:05:02 -0500

    Creando proyecto api client

---
+++ api-client/src/test/java/org/personales/apiclient/ApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personales.apiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
