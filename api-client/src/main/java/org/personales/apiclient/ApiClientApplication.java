15060db2f541caa0a9f66d10b7bc3631f68e1e1b
jotalvaro7 <jotalvaro.cc@gmail.com>
2023-02-12 12:36:06 -0500

    inyectando estereotipo no es necesario pero es buena practica

--- api-client/src/main/java/org/personales/apiclient/ApiClientApplication.java
+++ api-client/src/main/java/org/personales/apiclient/ApiClientApplication.java
@@ -2,9 +2,11 @@ package org.personales.apiclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiClientApplication {
 
@@ -13,3 +15,4 @@ public class ApiClientApplication {
 	}
 
 }
+ 
