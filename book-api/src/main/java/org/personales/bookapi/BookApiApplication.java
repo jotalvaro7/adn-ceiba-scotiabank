4864c9e2e14527db7d2d52295f5d4cba25559fa9
jotalvaro7 <jotalvaro.cc@gmail.com>
2023-03-27 22:44:45 -0500

    implementando Ribbon en las 2 APIS. Tener en cuenta que para el manejo de Ribbon es necesario bajar la version de spring boot y cambiar la version de spring cloud (Ya que netflix no esta dando mas soporte a partir de la version 2.4 en adelante a esta tecnologia)

--- book-api/src/main/java/org/personales/bookapi/BookApiApplication.java
+++ book-api/src/main/java/org/personales/bookapi/BookApiApplication.java
@@ -3,9 +3,11 @@ package org.personales.bookapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
-@SpringBootApplication
+@RibbonClient(name = "msvc-book-api")
 @EnableEurekaClient
+@SpringBootApplication
 public class BookApiApplication {
 
 	public static void main(String[] args) {
--- rating-api/src/main/java/org/personales/ratingapi/RatingApiApplication.java
+++ rating-api/src/main/java/org/personales/ratingapi/RatingApiApplication.java
@@ -3,9 +3,11 @@ package org.personales.ratingapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
-@SpringBootApplication
+@RibbonClient(name = "msvc-rating-api")
 @EnableEurekaClient
+@SpringBootApplication
 public class RatingApiApplication {
 
 	public static void main(String[] args) {
