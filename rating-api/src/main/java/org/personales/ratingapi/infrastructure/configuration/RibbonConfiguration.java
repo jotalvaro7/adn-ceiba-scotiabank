9faed7adaf891d22b79dcaf1a1290a0de0f1f2a5
jotalvaro7 <jotalvaro.cc@gmail.com>
2023-03-28 22:10:01 -0500

    Configurando Ribbon en las API y ajustando el docker compose para levantar otra instancia de la aplicacion

---
+++ rating-api/src/main/java/org/personales/ratingapi/infrastructure/configuration/RibbonConfiguration.java
@@ -0,0 +1,24 @@
+package org.personales.ratingapi.infrastructure.configuration;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RibbonConfiguration {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public IRule ribbonRule(){
+        return new WeightedResponseTimeRule();
+    }
+
+}
